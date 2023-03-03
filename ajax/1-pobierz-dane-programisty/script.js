221343b7ccb47f46a12a0cf37877d8b7d383dc88
karolina269 <120135313+karolina269@users.noreply.github.com>
2023-03-03 15:55:18 +0100

    ajax-corrected

--- ajax/1-pobierz-dane-programisty/script.js
+++ ajax/1-pobierz-dane-programisty/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
   $("button").click(function () {
-    $(this).after($('<div id="dane-programisty"></div>'));
-    $("#dane-programisty").load("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php");
+    $.get("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php")
+      .done(function (data) {
+        $("button").after($('<div id="dane-programisty"></div>'));
+        const dataObj = JSON.parse(data);
+        $.each(dataObj, function (key, value) {
+          $("#dane-programisty").append($(`<p>${key}: ${value}</p>`));
+        });
+      })
+      .fail(function (error) {
+        console.error(error);
+      });
   });
+  // $("button").click(function () {
+  //   $(this).after($('<div id="dane-programisty"></div>'));
+  //   $("#dane-programisty").load("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php");
+  // });
 });
